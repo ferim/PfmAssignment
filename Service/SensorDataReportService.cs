3134e041facd5ee2d852db164a46daed059fcc6c
Fatih Erim <11525841+ferim@users.noreply.github.com>
2023-10-14 19:02:13 +0200

    GetSensorDataReport method filled
    Hourly and daily groping is done. Weekly missing.

--- Service/SensorDataReportService.cs
+++ Service/SensorDataReportService.cs
@@ -13,7 +13,36 @@ namespace Service
         }
         public SensorDataReportResponseModel GetSensorDataReport(string fullPath)
         {
+            var data = _sensorDataReportRepository.GetSensorData(fullPath);
+
+            var hourlyReport = data.GroupBy(c =>
+                new
+                {
+                    Year = c.DateFrom.Year,
+                    Month = c.DateFrom.Month,
+                    Day = c.DateFrom.Day,
+                    Hour = c.DateFrom.Hour,
+                }).Select(x => new
+                {
+                    Time = new DateTime(x.Key.Year, x.Key.Month, x.Key.Day, x.Key.Hour, 0, 0),
+                    TotalCount = x.Sum(v => v.Count)
+                });
+
+            var dailyReport = data.GroupBy(c =>
+                new
+                {
+                    Year = c.DateFrom.Year,
+                    Month = c.DateFrom.Month,
+                    Day = c.DateFrom.Day
+                }).Select(x => new
+                {
+                    Time = new DateTime(x.Key.Year, x.Key.Month, x.Key.Day, 0, 0, 0),
+                    TotalCount = x.Sum(v => v.Count)
+                });
+
+            var weeklyReport = "";//Todo
             return new SensorDataReportResponseModel();
         }
+       
     }
 }
